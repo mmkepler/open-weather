9da5638a19893522df574eba65a80c78bba0aedf
Melissa Kepler <mmkepler@yahoo.com>
2019-07-29 12:55:53 -0400

    fix: several items for Lighthouse audit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,12 +4,12 @@ const Footer = (props) => (
   <footer className='w-100'>
     <div className='row'>
       <div className='col-6'>
-        <p id='footer-link'>
+        <p className='footer-link'>
           <a href="http://mmkepler.com" target="_blank" rel="noopener noreferrer">Melissa Kepler</a>
         </p>
       </div>
       <div className='col-6'>
-        <p id='footer-link'>
+        <p className='footer-link'>
           <a href="https://github.com/Missarachnid/open-weather" target="_blank" rel="noopener noreferrer">GitHub</a>
         </p>
       </div>
