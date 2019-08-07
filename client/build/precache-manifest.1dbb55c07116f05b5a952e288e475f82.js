007de6b29fa2204ca7547750e11cd755db028b37
Melissa Kepler <mmkepler@yahoo.com>
2019-08-07 19:55:47 -0400

    feat: build

---
+++ client/build/precache-manifest.1dbb55c07116f05b5a952e288e475f82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968f527341bc84eca870a2ab78b27207",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a3bf2ecd7539ab864b",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "bfa332fe3984e3994a0c",
+    "url": "/static/css/main.910ab67e.chunk.css"
+  },
+  {
+    "revision": "37a3bf2ecd7539ab864b",
+    "url": "/static/js/2.f068c800.chunk.js"
+  },
+  {
+    "revision": "bfa332fe3984e3994a0c",
+    "url": "/static/js/main.29677d4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4832cbcb3788921f88de78faec0d9de",
+    "url": "/static/media/hohenschwangau-bg-4.d4832cbc.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
